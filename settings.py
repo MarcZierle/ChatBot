82a4912535c7736e91e8a3f6f740f81ceca9dc12
MarcZierle <online@marczierle.com>
2019-05-15 18:56:15 +0200

    simple inital structure and google distance matrix API embedding

---
+++ settings.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+def init():
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path, verbose=True)
+
+    global GOOGLE_DISTANCE_MATRIX_API_KEY, TELEGRAM_API_KEY
+    GOOGLE_DISTANCE_MATRIX_API_KEY  = os.getenv("GOOGLE_DISTANCE_MATRIX_API_KEY")
+    TELEGRAM_API_KEY                = os.getenv("TELEGRAM_API_KEY")
