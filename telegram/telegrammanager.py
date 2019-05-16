e6a5be9d835e927bef647b9b58a20f738d50cf52
MarcZierle <online@marczierle.com>
2019-05-16 11:32:29 +0200

    added inteface for the telegram manager module

--- telegram/telegrammanager.py
+++ telegram/telegrammanager.py
@@ -1,2 +1,15 @@
 class TelegramManager():
-    
+
+    def __init__(self, api_key):
+
+
+    def restore(self):
+
+
+    def fetchnewmessages(self):
+
+
+    def getnewmessages(self):
+
+
+    def sendmessage(self, userid, msg)
