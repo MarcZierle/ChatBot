6f8050cc807c3d3e949f98713df079fa574e7eae
Local-Admin <ladmin@templ-linux64-debian-9.2.1>
2019-06-01 10:38:06 +0200

    changed loading path of env file

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ import os, logging, atexit
 
 def init():
     env_path = Path('.') / '.env'
-    load_dotenv(dotenv_path=env_path, verbose=True)
-    #load_dotenv(".env", verbose=True)
+    #load_dotenv(dotenv_path=env_path, verbose=True)
+    load_dotenv(".env", verbose=True)
 
     global GOOGLE_DISTANCE_MATRIX_API_KEY, TELEGRAM_API_KEY
     GOOGLE_DISTANCE_MATRIX_API_KEY  = os.getenv("GOOGLE_DISTANCE_MATRIX_API_KEY")
