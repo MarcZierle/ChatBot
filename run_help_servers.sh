5f7c1265f29c151c2d61375c6d193484e2715150
Marc Zierle <online@marczierle.com>
2019-07-10 19:30:58 +0200

    changed few files for the server

--- run_help_servers.sh
+++ run_help_servers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd duckling
-stack exec duckling-example-exe &
+../stack/stack exec duckling-example-exe &
 
 cd ../rasa/
 rasa run actions --actions actions
 
-sudo pkill duckling
+pkill duckling
