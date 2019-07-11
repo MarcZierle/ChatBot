5a873f31230fe1b231c3048f9bbb645db7f201eb
Marc Zierle <online@marczierle.de>
2019-07-11 13:11:41 +0200

    remade schedule layout

--- planner/plannertoimage.py
+++ planner/plannertoimage.py
@@ -9,19 +9,25 @@ class PlannerToImage():
     __WIDTH     = 1754
     __HEIGHT    = 1240
 
+    bot_name = "JANUS"
+    additional_info_text = [
+        bot_name+". An event planning ChatBot with respect to travel times and durations.",
+        "Created as part of the semester project \"Dialoge mit Computern\" at Humboldt University of Berlin."
+    ]
+
     def __init__(self, planner, day):
         self.__planner      = planner
         self.__first_day    = self.__get_monday(day)
         self.__time_start   = planner.get_time_begin_day()
         self.__time_end     = planner.get_time_end_day()
-        path = os.path.dirname(__file__)
-        self.__header_font      = ImageFont.truetype(path+"/Dosis-Bold.ttf", size=int(PlannerToImage.__HEIGHT/20))
-        self.__subtitle_font    = ImageFont.truetype("Ubuntu-B.ttf", size=int(PlannerToImage.__HEIGHT/49.6))
-        self.__top_font         = ImageFont.truetype("Ubuntu-B.ttf", size=int(PlannerToImage.__HEIGHT/60))
-        self.__top_font_R       = ImageFont.truetype("Ubuntu-R.ttf", size=int(PlannerToImage.__HEIGHT/60))
-        self.__regular_font     = ImageFont.truetype("Ubuntu-R.ttf", size=int(PlannerToImage.__HEIGHT/82))
-        self.__event_name_font  = ImageFont.truetype("Ubuntu-R.ttf", size=int(PlannerToImage.__HEIGHT/82))
-        self.__time_font        = ImageFont.truetype("Ubuntu-R.ttf", size=int(PlannerToImage.__HEIGHT/82))
+        self.__path         = os.path.dirname(__file__)
+        self.__header_font      = ImageFont.truetype(self.__path+"/../assets/fonts/Dosis-Bold.ttf", size=int(PlannerToImage.__HEIGHT/30))
+        self.__subtitle_font    = ImageFont.truetype(self.__path+"/../assets/fonts/Ubuntu-B.ttf", size=int(PlannerToImage.__HEIGHT/49.6))
+        self.__top_font         = ImageFont.truetype(self.__path+"/../assets/fonts/Ubuntu-B.ttf", size=int(PlannerToImage.__HEIGHT/60))
+        self.__top_font_R       = ImageFont.truetype(self.__path+"/../assets/fonts/Ubuntu-R.ttf", size=int(PlannerToImage.__HEIGHT/60))
+        self.__add_info_font    = ImageFont.truetype(self.__path+"/../assets/fonts/Ubuntu-R.ttf", size=int(PlannerToImage.__HEIGHT/82))
+        self.__event_name_font  = ImageFont.truetype(self.__path+"/../assets/fonts/Ubuntu-R.ttf", size=int(PlannerToImage.__HEIGHT/82))
+        self.__time_font        = ImageFont.truetype(self.__path+"/../assets/fonts/Ubuntu-R.ttf", size=int(PlannerToImage.__HEIGHT/82))
         self.__spaceing = PlannerToImage.__HEIGHT / 80
 
 
@@ -39,28 +45,90 @@ class PlannerToImage():
 
         self.img_left    = PlannerToImage.__WIDTH/14
         self.img_right   = PlannerToImage.__WIDTH - self.img_left
-        self.img_top     = PlannerToImage.__HEIGHT/14
+        self.img_top     = PlannerToImage.__HEIGHT/16
         self.img_bottom  = PlannerToImage.__HEIGHT - self.img_top
 
-        self.plan_top    = self.img_top + self.__header_font.getsize(".")[1] + 5*self.__spaceing + self.__subtitle_font.getsize(".")[1]
+        self.plan_top    = self.img_top + self.__header_font.getsize(PlannerToImage.bot_name)[0] + self.__header_font.getsize(PlannerToImage.bot_name)[1] + 3*self.__spaceing
 
-        now = dt.today()
-
-        draw.text((self.img_left,self.img_top), "JANUS Planning", font=self.__header_font, fill="black")
-        draw.text( (self.img_left,self.img_top + self.__header_font.getsize(".")[1] + self.__spaceing),
-            "Generated on " + now.strftime("%d/%m/%Y") + " at " + now.strftime("%I:%M %p"),
-            font=self.__subtitle_font, fill="black")
-
-        draw.rectangle([(self.img_left,self.plan_top), (self.img_right,self.img_bottom)],
-            fill="white", outline="gray", width=2)
-
+        self.__draw_header(img, draw)
         self.__draw_grid(draw)
         self.__draw_events(draw)
+        self.__draw_additional_infos(img, draw)
 
         img.save(file_name)
 
 
+    def __draw_header(self, img, draw):
+        logo_txt_w = int(self.__header_font.getsize(PlannerToImage.bot_name)[0])
+        logo_spaceing_left = 2*self.__spaceing
+
+        red_logo = Image.open(self.__path+"/../assets/images/logo_red_transparent.png")
+        red_logo = red_logo.resize((logo_txt_w, logo_txt_w), Image.ANTIALIAS)
+
+        pixel_data = red_logo.load()
+        if red_logo.mode == "RGBA":
+          for y in range(red_logo.size[1]):
+            for x in range(red_logo.size[0]):
+              if pixel_data[x, y][3] < 255:
+                pixel_data[x, y] = (255, 255, 255, 255)
+
+        img.paste(red_logo,
+            (int(self.img_left+logo_spaceing_left), int(self.img_top))
+        )
+
+        draw.text((self.img_left+logo_spaceing_left,self.img_top+logo_txt_w),
+            PlannerToImage.bot_name,
+            font=self.__header_font, fill="black")
+
+        #now = dt.today()
+        #draw.text( (self.img_left,self.img_top + self.__header_font.getsize(".")[1] + self.__spaceing),
+        #    "Generated on " + now.strftime("%d/%m/%Y") + " at " + now.strftime("%I:%M %p"),
+        #    font=self.__subtitle_font, fill="black")
+
+
+    def __draw_additional_infos(self, img, draw):
+        logo_size = int(0.7 *(PlannerToImage.__HEIGHT - self.img_bottom))
+
+        hu_logo = Image.open(self.__path+"/../assets/images/hu_logo.png")
+        hu_logo = hu_logo.resize((logo_size,logo_size), Image.ANTIALIAS)
+
+        pixel_data = hu_logo.load()
+        if hu_logo.mode == "RGBA":
+          for y in range(hu_logo.size[1]):
+            for x in range(hu_logo.size[0]):
+              if pixel_data[x, y][3] < 255:
+                pixel_data[x, y] = (255, 255, 255, 255)
+
+        hu_w, hu_h = hu_logo.size
+        bg_w, bg_h = img.size
+
+        logo_x = int(self.img_right-logo_size)
+        logo_y = int(self.img_bottom+(PlannerToImage.__HEIGHT - self.img_bottom)/2-logo_size/2)
+
+        img.paste(hu_logo,
+            (logo_x,logo_y)
+        )
+
+
+        for i in range(len(PlannerToImage.additional_info_text)):
+            txt = PlannerToImage.additional_info_text[i]
+            txt_w, txt_h = self.__add_info_font.getsize(txt)
+
+            line_h = txt_h+self.__spaceing/4
+            num_lines = len(PlannerToImage.additional_info_text)
+
+            xpos = self.img_right-logo_size-self.__spaceing-txt_w
+            ypos = logo_y+logo_size/2 + i*line_h - (num_lines*line_h)/2
+
+            draw.text( (xpos, ypos),
+                txt,
+                font=self.__add_info_font, fill="gray")
+
+
     def __draw_grid(self, draw):
+        draw.rectangle([(self.img_left,self.plan_top), (self.img_right,self.img_bottom)],
+            fill="white", outline="gray", width=2)
+
         self.events_top     = self.plan_top + 3*self.__spaceing + self.__top_font.getsize(".")[1]
         self.events_left    = self.img_left+6*self.__spaceing
 
