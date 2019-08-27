442c15238d5c18cc24f9fb7a0ce5a9ec18757619
LeonardFiedrowicz <fiedrowicz.leonard@gmail.com>
2019-08-27 19:35:37 +0200

    Validates time and duration

--- rasa/actions/plan_event_form.py
+++ rasa/actions/plan_event_form.py
@@ -1,15 +1,16 @@
-from typing import Any, Text, Dict, List, Union
+from typing import Any, Text, Dict, List, Union, Optional
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 
 from rasa.core.trackers import DialogueStateTracker
-from rasa.core.events import SlotSet
+
+import ast, json, datetime
+from datetime import datetime as dt
+
 
 class PlanEventForm(FormAction):
-
-    user_duration_replacements = {}
 
     def name(self):
         return "plan_event_form"
@@ -24,16 +25,6 @@ class PlanEventForm(FormAction):
             slots.append("duration")
         else :
             slots.append("is_specific")
-
-        user_id = str(tracker.current_state()["sender_id"])
-
-        if not user_id in PlanEventForm.user_duration_replacements:
-            PlanEventForm.user_duration_replacements[user_id] = None
-        
-        if not PlanEventForm.user_duration_replacements[user_id] and tracker.get_slot("duration"):
-            duration_entity = next((e for e in tracker.latest_message["entities"] if e["entity"] == "duration"), None)
-            if duration_entity:
-                PlanEventForm.user_duration_replacements[user_id] = str(duration_entity)
         
         return slots
 
@@ -46,10 +37,7 @@ class PlanEventForm(FormAction):
     ) -> List[Dict]:
         dispatcher.utter_message("Event planned.")
         
-        user_id = str(tracker.current_state()["sender_id"])
-        slot_value = PlanEventForm.user_duration_replacements[user_id]
-        del PlanEventForm.user_duration_replacements[user_id]
-        return [SlotSet("duration", slot_value)]
+        return []
 
     
     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
@@ -63,3 +51,40 @@ class PlanEventForm(FormAction):
             ],
             "event_name": self.from_text()
             }
+
+
+    def validate_duration(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Optional[Text]:
+
+        duration_entity = str(next((e for e in tracker.latest_message["entities"] if e["entity"] == "duration"), None))
+        if duration_entity:
+            duration_in_sec = int(ast.literal_eval(duration_entity)['additional_info']['normalized']['value']/60)
+            return {"duration": str(duration_in_sec)}
+        else:
+            return {"duration": None}
+
+
+    def validate_time(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Optional[Text]:
+
+        try:
+            time = dict(value)#json.loads(str(value).replace("'", '"'))
+            if 'to' in time or 'from' in time:
+                return {"time": str(time)}
+        except:
+            pass
+        return {"time": None}
+
+    
+    
+        
